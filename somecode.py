32f8f4f8b0681df42856fe73235aaaffbffd291c
Mark Tucker <mark@TheMac.local>
2018-03-31 07:11:58 -0400

    more edits #2

--- somecode.py
+++ somecode.py
@@ -6,10 +6,15 @@
 def main():
     pass
 
+def ReadData(xstream):
+    pass
+    # read in data from file handle
+
 class somethingelse():
     '''added a comment here'''
     def __init__(self, data):
-        pass
+        self.id = data['id']
+        self.date = data['date']
 
 
 main()
