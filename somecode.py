a0d30e57d1d884203ea5e1eb9e6744717c661e42
Mark Tucker <mark@TheMac.local>
2018-03-31 07:24:16 -0400

    added edits from second branch

--- somecode.py
+++ somecode.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# these comments are important and should be merged in
+
 # test script that doesn't do much.
 
 
@@ -10,11 +12,15 @@ def ReadData(xstream):
     pass
     # read in data from file handle
 
-class somethingelse():
+class somethingelse:
     '''added a comment here'''
     def __init__(self, data):
         self.id = data['id']
         self.date = data['date']
+
+class anotherclass:
+    pass
+    # stubbed out, but necessary
 
 
 main()
