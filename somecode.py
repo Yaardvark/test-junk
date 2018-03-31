8a27bc39e8211593662e1931647a936b63348bf3
Mark Tucker <mark@TheMac.local>
2018-03-31 07:18:13 -0400

    more edits #3

--- somecode.py
+++ somecode.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
+# these comments are important and should be merged in
+
 # test script that doesn't do much.
 
 
 def main():
     pass
 
-class somethingelse():
+class somethingelse:
     '''added a comment here'''
     def __init__(self, data):
         pass
 
+
+class anotherclass:
+    pass
+    # stubbed out, but necessary
 
 main()
